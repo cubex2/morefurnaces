395450c4e5e63d1bccc1349739bea5cd5fb66da7
cubex2 <breuer.t.42@gmail.com>
2017-10-08 11:51:22 +0200

    Bump version

--- src/main/java/cubex2/mods/morefurnaces/ModInformation.java
+++ src/main/java/cubex2/mods/morefurnaces/ModInformation.java
@@ -4,5 +4,5 @@ public class ModInformation
 {
     public static final String ID = "morefurnaces";
     public static final String NAME = "More Furnaces";
-    public static final String VERSION = "1.10.2";
+    public static final String VERSION = "1.10.3";
 }
